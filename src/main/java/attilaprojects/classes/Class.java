e30d9ececf1e4d1abe16c314e7345017c93c21c6
Gyorgy Attila <gyorgyattxla@gmail.com>
2024-11-08 15:42:44 +0100

    Started work on Class object.

--- src/main/java/attilaprojects/classes/Class.java
+++ src/main/java/attilaprojects/classes/Class.java
@@ -1,4 +1,10 @@
 package attilaprojects.classes;
 
 public class Class {
+    private String className;
+    private String classTeacher;
+    private int creditNumber;
+    private String classStartTime;
+    private String classEndTime;
+    private String[] assignedStudents;
 }
